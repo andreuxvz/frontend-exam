8442dc0dc0a6e5d9edb61cb2be7689972652f741
Andreu Ayaipoma Condori <andreu@multiplica.com>
2019-01-28 12:21:00 -0500

    dist for backend front in heroku bug 6

--- backend-front/server.js
+++ backend-front/server.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/../dist/'));
 app.get(/.*/, function(req, res) {
-	res.sendFile(__dirname + '/../dist/index.html');
+	var path = require("path");
+	res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
 app.listen(port);
 
