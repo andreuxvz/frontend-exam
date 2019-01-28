a5ed82105204d956312581dc6820436486684217
Andreu Ayaipoma Condori <andreu@multiplica.com>
2019-01-28 15:29:43 -0500

    adding json server

--- backend-front/server.js
+++ backend-front/server.js
@@ -2,11 +2,36 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+server.use(middlewares)
+//const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/../dist/'));
 app.get(/.*/, function(req, res) {
+	console.log(req, 'requestwqkrowerwe wqernwrmwer')
 	var path = require("path");
 	res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
-app.listen(port);
 
+module.exports = (req, res, next) => {
+  if (req.method === 'POST' && req.path === '/login/') {
+    if (req.body.email === 'admin@vue.com' && req.body.password === 'secret') {
+      res.status(200).json({
+        message: 'successful login'
+      })
+    } else {
+      res.status(400).json({
+        message: 'wrong password',
+        data: req.body
+      })
+    }
+  } else {
+    next();
+  }
+}
+
+app.listen(port);
 console.log('server is running ok', __dirname + '../dist/');
