9a2bc0b0bd0017d812e3d2f53db62dc477f05bee
Andreu Ayaipoma Condori <andreu@multiplica.com>
2019-01-28 12:17:27 -0500

    dist for backend front in heroku bug 5

--- backend-front/server.js
+++ backend-front/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '../dist/'));
+app.use(express.static(__dirname + '/../dist/'));
 app.get(/.*/, function(req, res) {
-	res.sendFile(__dirname + '../dist/index.html');
+	res.sendFile(__dirname + '/../dist/index.html');
 });
 app.listen(port);
 
-console.log('server is running ok');
+console.log('server is running ok', __dirname + '../dist/');
